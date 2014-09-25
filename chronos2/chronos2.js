490c60e98311e71a1b5aeb9d914fe158b40b520e
sturtus <sturtus@gmail.com>
2014-09-24 20:58:39 -0400

    chronos2 initial page

---
+++ chronos2/chronos2.js
@@ -0,0 +1,73 @@
+/* 
+
+A rewrite of  chronos
+
+Key Features and Functions
+
+
+
+*/
+
+
+// **********  CONFIGURATION   *******************************************************
+
+// define user configurable variables
+
+// **********  DATABASE   *******************************************************
+
+// populate database
+
+
+// **********  MAIN FUNCTIONS  *******************************************************
+
+//initialize chronos
+//generate character journal CHRONOS-config
+//generate macros/abilities as token actions for character
+//generate page CHRONOS-page
+// ? format page with fun stuff, such as current date and time or other data?
+//create token CHRONOS-control on page
+//associate token with journal entry
+// ? prompt for current time to set?
+
+//add event
+
+//increment time
+
+//diplay time
+
+//display events (with modal to determine type)
+
+//set condition
+
+//remove condition (with modal to determine type)
+
+//display conditions
+
+
+
+
+// **********  EVENT LISTENERS  *******************************************************
+
+// end of round
+// x time passed
+
+
+// **********  UTILITY FUNCTIONS   *******************************************************
+
+//modal prompt
+//generates a popup to enter parameters for a chosen function. displays options, requests entry.
+//accept parameters: array of options in string format
+//sends macro to chat with array, one entry per line from array with ? to prompt for user input.
+//returns string user entered or 0 for nothing entered.
+
+//set marker
+//remove marker
+//set light
+//remove light
+//set aura
+//remove aura
+//set tint
+//remove tint
+//write text on page background
+//print to CHRONOS journal entry
+
